1187ff83ccfc0788e22c37035c51595d211c1f28
NashidC <nash.chowdhury@gmail.com>
2017-02-02 00:18:49 -0500

    update

--- Flickster/MoviesViewController.swift
+++ Flickster/MoviesViewController.swift
@@ -16,49 +16,68 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     @IBOutlet weak var tableView: UITableView!
     
     var movies: [NSDictionary]?
-    var refreshControl:UIRefreshControl!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-       let refreshControl = UIRefreshControl()
-        refreshControl.addTarget(self, action: #selector(MoviesViewController.networkReq), for: .valueChanged)
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(refreshControlAction(_:)), for: UIControlEvents.valueChanged)
         
         tableView.insertSubview(refreshControl, at: 0)
         tableView.dataSource = self
         tableView.delegate = self
         
-       
-    }
-    
-    func networkReq() {
-    let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+        let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")!
         
         MBProgressHUD.showAdded(to: self.view, animated: true)
+        
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
         let task: URLSessionDataTask = session.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
             
-            MBProgressHUD.hide(for: self.view, animated: true)
-            
+         
             if let data = data {
                 if let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
                     print(dataDictionary)
                     
-                    self.tableView.reloadData()
-                    self.refreshControl.endRefreshing()
                     self.movies = (dataDictionary["results"] as! [NSDictionary])
                     self.tableView.reloadData()
                 }
             }
+             MBProgressHUD.hide(for: self.view, animated: true)
         }
         task.resume()
 
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    
+    func refreshControlAction(_ refreshControl: UIRefreshControl) {
+        
+        let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task: URLSessionDataTask = session.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
+            if let data = data {
+                if let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
+                    print(dataDictionary)
+                    
+                    self.movies = dataDictionary["results"] as? [NSDictionary]
+                    self.tableView.reloadData()
+                }
+            }
+            
+            
+            self.tableView.reloadData()
+            
+            refreshControl.endRefreshing()
+        }
+        task.resume()
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -101,7 +120,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         return cell
         
     }
-}
+    
 
     /*
     // MARK: - Navigation
@@ -113,4 +132,4 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     */
 
-
+}
